663d1bfd4cd6538f49218a20f2e453df6c07e0b9
lybc <yibocheng.li@gmail.com>
2020-07-20 17:01:01 +0800

    修改安装脚本判断

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
-if [ ! -d "~/.vim"  ];then
+ls ~/.vim
+if [ -d "~/.vim" ];then
     mkdir ~/.vim
 fi
-   
+
+if [ -f "~/.vim/bundle/Vundle.vim" ]; then
+    echo "Vundle.vim不存在，开始安装..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 # install vundle    
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/lybc/vim.git /tmp/vim
 
 cp -rf ./vimrc ~/.vimrc
@@ -13,4 +16,5 @@ cp -rf ./ideavimrc ~/.ideavimrc
 cp -rf ./colors ~/.vim/colors
 
 vim -E -u $HOME/.vimrc +qall
+rm -rf /tmp/vim
 echo 'Install Complete!'
